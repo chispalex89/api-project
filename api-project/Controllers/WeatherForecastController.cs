23435385c9c2c01e7bd5fa3eda1f006c388870f9
Pablo Godoy <pablogodoy@azlo.com>
2020-01-24 12:26:30 -0600

    Change on folder and project name

---
+++ api-project/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using api_proyect.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_proyect.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id?}")]
+        public IEnumerable<WeatherForecast> Obtener(int id = -1)
+        {
+            if(id > -1)
+            {
+                return id > Data.Instance.numbers.Count
+                    ? new List<WeatherForecast>()
+                    : new List<WeatherForecast>() { Data.Instance.weatherForecasts[id] };
+            }
+            
+            return Data.Instance.weatherForecasts;
+        }
+
+        [HttpPost]
+        public WeatherForecast Agregar([FromBody]WeatherForecast weatherForecast)
+        {
+            Data.Instance.weatherForecasts.Add(weatherForecast);
+            return weatherForecast;
+        }
+    }
+}
---
+++ api-project/Helpers/Data.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace api_proyect.Helpers
+{
+    public class Data
+    {
+        private static Data _instance = null;
+
+        public static Data Instance {
+            get
+            {
+                if (_instance == null) _instance = new Data();
+                return _instance;
+            }
+        }
+
+        public string name;
+        public int value;
+        public bool isChecked;
+        public List<int> numbers = new List<int>();
+        public List<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+    }
+}
